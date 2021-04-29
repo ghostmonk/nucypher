dc2c44dcda6f9141ef37b3cb90616c5885212bdd
ghostmonk <nicholas@ghostmonk.com>
2021-04-29 19:27:05 -0400

    la la la

--- examples/hackathon-demo/alicia.py
+++ examples/hackathon-demo/alicia.py
@@ -1,12 +1,15 @@
 import datetime
 import json
 import os
+import random
 import shutil
 import subprocess
+import time
 
 import maya
+import msgpack
 
-from nucypher.characters.lawful import Bob, Ursula
+from nucypher.characters.lawful import Bob, Ursula, Enrico
 from nucypher.config.characters import AliceConfiguration
 from nucypher.config.constants import TEMPORARY_DOMAIN
 from nucypher.utilities.logging import GlobalLoggerSettings
@@ -16,6 +19,7 @@ TEMP_ALICE_DIR = os.path.join('/', 'tmp', 'hackathon')
 SEEDNODE_URI = "localhost:11500"
 POLICY_FILENAME = "policy-metadata.json"
 passphrase = "TEST_ALICIA_INSECURE_DEVELOPMENT_PASSWORD"
+HEART_DATA_FILENAME = 'heart_data.msgpack'
 
 shutil.rmtree(TEMP_ALICE_DIR, ignore_errors=True)
 
@@ -43,29 +47,66 @@ alicia = alice_config.produce()
 alice_config_file = alice_config.to_configuration_file()
 print("Alice Config saved")
 subprocess.run(["ccat", "/tmp/hackathon/alice.json"])
+subprocess.run(["echo", "\r"])
 subprocess.run(["tree", "/tmp/hackathon"])
 input("Alice has Ursula Seed TLS")
 
 alicia.start_learning_loop(now=True)
+subprocess.run(["tree", "/tmp/hackathon/known_nodes"])
 input("Alicia Knows about the Ursula Network")
 
 label = "heart-data-❤️-"+os.urandom(4).hex()
+print("Label generated for IOT data: ", label)
 label = label.encode()
+print("Label encoded: ", label)
 
 policy_pubkey = alicia.get_policy_encrypting_key_from_label(label)
+data_source = Enrico(policy_encrypting_key=policy_pubkey)
+data_source_public_key = bytes(data_source.stamp)
+input("Enrico set up to accept Data for Encryption, Time to generate heart-rate data.")
 
-import heart_monitor
-heart_monitor.generate_heart_rate_samples(policy_pubkey, samples=50, save_as_file=True)
-input("Data generated from phone")
+heart_rate = 80
+now = time.time()
+beats = list()
+
+for _ in range(50):
+    heart_rate = random.randint(max(60, heart_rate - 5), min(100, heart_rate + 5))
+    now += 3
+
+    beats.append({
+        'heart_rate': heart_rate,
+        'timestamp': now,
+    })
+
+
+data = {
+    'heartbeats': beats,
+}
+
+with open('alice_heart_data.json', 'w') as outfile:
+    json.dump(data, outfile)
+
+subprocess.run(["jq", ".", "alice_heart_data.json"])
+
+with open('alice_heart_data.json', 'rb') as file:
+    plain_bytes = file.read()
+    encrypted_msg, _signature = data_source.encrypt_message(plain_bytes)
+
+with open('alice_heart_data.enc', 'wb') as enc_out:
+    enc_out.write(encrypted_msg.to_bytes())
+
+subprocess.run(["ccat", "alice_heart_data.enc"])
+
+input("Continue")
 
 from doctor_keys import get_doctor_pubkeys
 doctor_pubkeys = get_doctor_pubkeys()
-input("Doctor Public keys generated")
+print("Doctor Public keys generated")
 
-
-doctor_strange = Bob.from_public_keys(verifying_key=doctor_pubkeys['sig'],
-                                      encrypting_key=doctor_pubkeys['enc'],
-                                      federated_only=True)
+doctor_strange = Bob.from_public_keys(
+    verifying_key=doctor_pubkeys['sig'],
+    encrypting_key=doctor_pubkeys['enc'],
+    federated_only=True)
 
 # Here are our remaining Policy details, such as:
 # - Policy expiration date
