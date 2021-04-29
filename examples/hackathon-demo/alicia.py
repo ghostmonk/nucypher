2bd1dbf075f0d91b5c7b0b94b96da06ddc40783b
ghostmonk <nicholas@ghostmonk.com>
2021-04-29 17:12:42 -0400

    cha cha cha changes

--- examples/hackathon-demo/alicia.py
+++ examples/hackathon-demo/alicia.py
@@ -1,25 +1,8 @@
-"""
- This file is part of nucypher.
-
- nucypher is free software: you can redistribute it and/or modify
- it under the terms of the GNU Affero General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- nucypher is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU Affero General Public License for more details.
-
- You should have received a copy of the GNU Affero General Public License
- along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
-"""
-
 import datetime
-import sys
 import json
 import os
 import shutil
+import subprocess
 
 import maya
 
@@ -28,48 +11,15 @@ from nucypher.config.characters import AliceConfiguration
 from nucypher.config.constants import TEMPORARY_DOMAIN
 from nucypher.utilities.logging import GlobalLoggerSettings
 
-
-######################
-# Boring setup stuff #
-######################
-
-
-# Twisted Logger
-
 GlobalLoggerSettings.start_console_logging()
-
-TEMP_ALICE_DIR = os.path.join('/', 'tmp', 'heartbeat-demo-alice')
-
-
-# if your ursulas are NOT running on your current host,
-# run like this: python alicia.py 172.28.1.3:11500
-# otherwise the default will be fine.
-
-try:
-    SEEDNODE_URI = sys.argv[1]
-except IndexError:
-    SEEDNODE_URI = "localhost:11500"
-
+TEMP_ALICE_DIR = os.path.join('/', 'tmp', 'hackathon')
+SEEDNODE_URI = "localhost:11500"
 POLICY_FILENAME = "policy-metadata.json"
-
-
-#######################################
-# Alicia, the Authority of the Policy #
-#######################################
-
-
-# We get a persistent Alice.
-# If we had an existing Alicia in disk, let's get it from there
-
 passphrase = "TEST_ALICIA_INSECURE_DEVELOPMENT_PASSWORD"
-# If anything fails, let's create Alicia from scratch
-# Remove previous demo files and create new ones
 
 shutil.rmtree(TEMP_ALICE_DIR, ignore_errors=True)
 
-ursula = Ursula.from_seed_and_stake_info(seed_uri=SEEDNODE_URI,
-                                         federated_only=True,
-                                         minimum_stake=0)
+ursula = Ursula.from_seed_and_stake_info(seed_uri=SEEDNODE_URI, federated_only=True, minimum_stake=0)
 
 alice_config = AliceConfiguration(
     config_root=os.path.join(TEMP_ALICE_DIR),
@@ -79,48 +29,40 @@ alice_config = AliceConfiguration(
     federated_only=True,
     learn_on_same_thread=True,
 )
-
 alice_config.initialize(password=passphrase)
 
+print("Alice has been initialized")
+subprocess.run(["tree", "/tmp/hackathon"])
+input("Continue")
+
 alice_config.keyring.unlock(password=passphrase)
+print("Alice Key Unlocked - ready to encrypt")
+input("Continue")
+
 alicia = alice_config.produce()
-
-# We will save Alicia's config to a file for later use
 alice_config_file = alice_config.to_configuration_file()
+print("Alice Config saved")
+subprocess.run(["ccat", "/tmp/hackathon/alice.json"])
+subprocess.run(["tree", "/tmp/hackathon"])
+input("Alice has Ursula Seed TLS")
 
-# Let's get to learn about the NuCypher network
 alicia.start_learning_loop(now=True)
+input("Alicia Knows about the Ursula Network")
 
-# At this point, Alicia is fully operational and can create policies.
-# The Policy Label is a bytestring that categorizes the data that Alicia wants to share.
-# Note: we add some random chars to create different policies, only for demonstration purposes
 label = "heart-data-❤️-"+os.urandom(4).hex()
 label = label.encode()
 
-# Alicia can create the public key associated to the policy label,
-# even before creating any associated policy.
 policy_pubkey = alicia.get_policy_encrypting_key_from_label(label)
 
-print("The policy public key for "
-      "label '{}' is {}".format(label.decode("utf-8"), policy_pubkey.to_bytes().hex()))
-
-# Data Sources can produce encrypted data for access policies
-# that **don't exist yet**.
-# In this example, we create a local file with encrypted data, containing
-# heart rate measurements from a heart monitor
 import heart_monitor
-heart_monitor.generate_heart_rate_samples(policy_pubkey,
-                                          samples=50,
-                                          save_as_file=True)
+heart_monitor.generate_heart_rate_samples(policy_pubkey, samples=50, save_as_file=True)
+input("Data generated from phone")
 
-
-# Alicia now wants to share data associated with this label.
-# To do so, she needs the public key of the recipient.
-# In this example, we generate it on the fly (for demonstration purposes)
 from doctor_keys import get_doctor_pubkeys
 doctor_pubkeys = get_doctor_pubkeys()
+input("Doctor Public keys generated")
 
-# We create a view of the Bob who's going to be granted access.
+
 doctor_strange = Bob.from_public_keys(verifying_key=doctor_pubkeys['sig'],
                                       encrypting_key=doctor_pubkeys['enc'],
                                       federated_only=True)
